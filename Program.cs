4167f403b18a66c8458035056a20e16f904309ca
Lígia Viana Azevedo <ligia.azevedo@sensedia.local>
2022-12-26 16:57:27 -0300

    classes-cs

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+       int[] numbers = {1, 4, 10, 42};
+
+       var query = from number in numbers
+                   where number < 10
+                   select number; 
+
+        var query2 = numbers.Where(number => number < 10);
+
+        Console.WriteLine(query.Count());
+    }
+}
+
+delegate int Calculate(int x, int y);
+
+
+
+
+class BankAccount 
+{
+    private string name;
+    private decimal balance;
+
+    public decimal Balance 
+    { 
+        get { return balance; }  
+
+    }
+
+    public BankAccount(string name, decimal balance)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            throw new Exception("Nome inválido");
+        }
+        if (balance < 0)
+        {
+            throw new Exception("Saldo não pode ser negativo");
+        }
+
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            return;
+        }
+        balance += amount;
+    }
+
+
+}
+
+
+ 
+
+
+
+
+
+
+
